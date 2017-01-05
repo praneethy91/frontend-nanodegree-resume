aab3ae27ab9d3a08ffde10e4cc5247fe8adad7c4
praneethy91 <praneethy91@gmail.com>
2017-01-05 13:44:06 -0500

    Changing the content of one of the project to point to right location and modifying description

--- src/js/resumeBuilder.js
+++ src/js/resumeBuilder.js
@@ -137,6 +137,7 @@ var projects = {
             dates: "2016",
             description: [
                 "This website was build using a responsive grid-based layout utilizing Bootstrap. This website works equally well across all screen sizes and devices in a responsive way.",
+                "The online resume website is generated using data from json so it's very easy to create your own resume by just changing the json.",
                 "Semantic elements have been used throughout the website. The images and fonts used in the website are themselves are responsive.",
                 "Images have been optimized using srcset and compression. Serve different image resolutions and quality according to viewport width and relative width of image in the viewport in a responsive manner.",
                 "A grunt workflow has been incorporated while building the website, utilizing various grunt plugins to optimize the development workflow and performance of the website.",
@@ -144,7 +145,7 @@ var projects = {
                 "Advanced components of Bootstrap were incorporated into the website like modals and navbar. Optimized website and resource fetching for website to improve Google pagespeed scores."
             ],
             image: "src/img/project3.png",
-            url: "https://github.com/praneethy91/mygithubpage"
+            url: "https://github.com/praneethy91/frontend-nanodegree-resume"
         }
     ],
     /**
